caed51de9e0c81eb536e391af4d25971e86fb7fb
alizzem <112910347+alizzem@users.noreply.github.com>
2022-12-01 18:42:32 +0400

    first

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactDOM } from "react";
+import "./style2.css"
+
+class Header extends React.Component{
+    render(){
+       return(
+            <>
+                <h1>Hello</h1>
+            </>
+       )
+    }
+}
+
+export default Header
